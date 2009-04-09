f45224659a5b50792429cafd01b0ab8737932dc4
audiohacked <audiohacked@aaba38f1-4247-0410-9621-85bd965dc335>
2009-04-09 23:26:17 +0000

    actually add the files...
    git-svn-id: https://audiohacked.googlecode.com/svn/trunk@103 aaba38f1-4247-0410-9621-85bd965dc335

---
+++ ps2mc/ps2_mc.c
@@ -0,0 +1,24 @@
+#include <usb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "usb_adaptor.h"
+#include "ps2_mc.h"
+
+int mc2rw_read(void)
+{
+	return 0;
+}
+
+int mc2rw_write(void)
+{
+	return 0;
+}
+
+int mc2rw_erase(void)
+{
+	return 0;
+}
